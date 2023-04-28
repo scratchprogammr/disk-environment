95710efa303115e1e0384e0e7f6fefdb09daa06d
scratchprogammr <131555910+scratchprogammr@users.noreply.github.com>
2023-04-28 15:31:52 -0400

    core.py

---
+++ core.py
@@ -0,0 +1,10 @@
+corevar="dgqwyteCXd xn mVYE dy*(B  &*  Y h U U DUD EUd zekj jjfwh AHI9 ha89"
+if (corevar=="dgqwyteCXd xn mVYE dy*(B  &*  Y h U U DUD EUd zekj jjfwh AHI9 ha89"):
+ print('working')
+else:
+  print('D: \n the "corevar" varible was not \n with its correct value \n errorcode: core_var_broken'
+def core(corevar):
+  while True:
+          int(0)
+        break
+ core(corevar)
